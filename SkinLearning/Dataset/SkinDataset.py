75a2ad2dd6f243c7682dd2b42941f331d7f4ef26
Robbie Southam <rjsoutham@gmail.com>
2023-03-20 13:00:38 +0000

    Testing wavelet decomposition, improving RNN

--- SkinLearning/Dataset/SkinDataset.py
+++ SkinLearning/Dataset/SkinDataset.py
@@ -4,25 +4,22 @@ import numpy as np
 import pandas as pd
 from scipy.interpolate import interp1d
 from torch import FloatTensor, tensor
-from torch.cuda import FloatTensor
+from torch.cuda import FloatTensor as GPUFloatTensor
 from tqdm import tqdm
 from torch.utils.data import Dataset
+
+from SkinLearning.Utils.NN import DEVICE
 
 # Folder name will correspond to index of sample
 class SkinDataset(Dataset):
     def __init__(self, scaler, signalFolder="D:/SamplingResults2", sampleFile="../Data/newSamples.pkl", runs=range(65535), steps=128):
         # Load both disp1 and disp2 from each folder
         # Folders ordered according to index of sample
-        # Use the corresponding sample as y -> append probe?
         self.input = []
         self.output = []
         
         with open(f"{sampleFile}", "rb") as f:
              samples = pickle.load(f)
-        
-        self.min = np.min(samples[runs])
-        self.max = np.max(samples[runs])
-        
         
         for run in tqdm(runs):
             inp = []
@@ -44,8 +41,7 @@ class SkinDataset(Dataset):
 
                 # Interpolate curve for consistent x values
                 xNew = np.linspace(0, 7, num=steps, endpoint=False)
-                interped = interp1d(a['x'], a['y'], kind='cubic', fill_value="extrapolate")(xNew)
-                    
+                interped = interp1d(a['x'], a['y'], kind='cubic', fill_value="extrapolate")(xNew)        
                 
                 inp.append(interped.astype("float32"))
             
@@ -56,10 +52,16 @@ class SkinDataset(Dataset):
                 self.input.append(inp)
                 self.output.append(samples[int(run)])
         
+        # Normalise output variables
         scaler.fit(self.output)
         self.output = scaler.fit_transform(self.output)
-        self.output = tensor(self.output).type(FloatTensor)
-        self.input = tensor(np.array(self.input)).type(FloatTensor)
+        
+        self.output = tensor(self.output).type(
+            FloatTensor if DEVICE == 'cpu' else GPUFloatTensor
+        )
+        self.input = tensor(np.array(self.input)).type(
+            FloatTensor if DEVICE == 'cpu' else GPUFloatTensor
+        )
         
     def __len__(self):
         return len(self.output)
